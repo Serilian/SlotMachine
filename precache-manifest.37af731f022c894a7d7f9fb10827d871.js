572bb78f0b9fffa82f2cbe28f2a81d406d9ccac6
fhagno@yahoo.com <fhagno@yahoo.com>
2019-08-10 12:25:38 +0200

    Updates

---
+++ precache-manifest.37af731f022c894a7d7f9fb10827d871.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aea092c5b944556030d8",
+    "url": "/SlotMachine/static/css/main.a6fef53c.chunk.css"
+  },
+  {
+    "revision": "aea092c5b944556030d8",
+    "url": "/SlotMachine/static/js/main.aea092c5.chunk.js"
+  },
+  {
+    "revision": "626f812dd9e3066df5bb",
+    "url": "/SlotMachine/static/js/1.626f812d.chunk.js"
+  },
+  {
+    "revision": "d3771922be22c05beac3",
+    "url": "/SlotMachine/static/js/runtime~main.d3771922.js"
+  },
+  {
+    "revision": "fded8d6365e14f5d17a4865fe0585439",
+    "url": "/SlotMachine/index.html"
+  }
+];
